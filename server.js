93df69db7dda539e9b6296ca5990a18cd31d371d
GaneshKumar Prakashbabu <ganeshrprakash@gmail.com>
2021-06-13 23:36:39 +0530

    Updated Node server to listen port

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/api/user', (req, res) => {
 
 
 // set port, listen for requests
-const port = process.env.port||'8080';
+const port = process.env.PORT ||'8080';
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
